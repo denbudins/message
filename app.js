596ac33280e1a8474ae6ffebdfd5e62320bf1c74
Denis Martin Budinski <denismartinbudinski@MacBook-Pro-od-Denis.local>
2020-12-08 18:50:36 +0100

    Finished app

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+const { hash } = window.location;
+
+const messege = atob(hash.replace('#', ''));
+
+if (messege){
+    document.querySelector('#messege-form').classList.add('hide');
+    document.querySelector('#messege-show').classList.remove('hide');
+
+    document.querySelector('h1').innerHTML = messege;
+}
+
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
+
+    document.querySelector('#messege-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
 
     const input = document.querySelector('#message-input');
     const encrypted = btoa(input.value);
 
-    document.querySelector('#link-input').value = encrypted;
+    const inputMessege = document.querySelector('#link-input');
+    inputMessege.value = `${window.location}#${encrypted}`;
+    inputMessege.select();
 });
